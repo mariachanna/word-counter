c8318eabae33a191e4f0ca10ac4e97975996042f
Maria Murad <147840185+mariachanna@users.noreply.github.com>
2024-04-21 17:09:00 +0500

    Add files via upload

---
+++ word.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+async function countWords() {
+    const userAns = await inquirer.prompt([
+        {
+            name: "sentence",
+            type: "input",
+            message: "Enter Your Sentence To Count The Words:"
+        }
+    ]);
+
+    const words = userAns.sentence.trim().split(" ");
+    console.log(words);
+
+    console.log(chalk.blueBright(`Your sentence word count is ${words.length}`));
+}
+
+countWords();
